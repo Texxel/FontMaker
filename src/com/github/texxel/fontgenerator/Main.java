a4b861a3d220ae1caf20a05a7caa2d1224ad0d3d
mdsimmo <mdsimmo2+git@gmail.com>
2015-12-15 21:47:37 +1100

    Created font files

--- src/com/github/texxel/fontgenerator/Main.java
+++ src/com/github/texxel/fontgenerator/Main.java
@@ -5,9 +5,11 @@ import java.io.File;
 public class Main {
 
     public static void main( String[] args ) throws Exception {
-        File file = new File( "data/font2.png" );
-        Font font = FontGenerator.makeFont( file, '!' );
-        FontGenerator.writeBmpFile( new File( "data/test.txt" ), font );
+        for ( int i = 1; i <= 5; i++ ) {
+            File file = new File( "data/font" + i + ".png" );
+            Font font = FontGenerator.makeFont( file, '!' );
+            FontGenerator.writeBmpFile( new File( "data/font" + i + ".fnt" ), font );
+        }
     }
 
 }
